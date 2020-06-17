102a5a3d20b5309010a88a8d81140da32985b479
Sravan <sravan.mk19@gmail.com>
2020-06-17 22:28:14 +0530

    C1

---
+++ NewPrjtWebDriver2.53Junit/src/packTestone/JUnit_Sample1.java
@@ -0,0 +1,42 @@
+package packTestone;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class JUnit_Sample1 
+{
+	@BeforeClass
+	public static void a1()
+	{
+		System.out.println("Before Method");
+	}
+	
+	@AfterClass
+	public static void b1()
+	{
+		System.out.println("After Method");
+	}
+	
+	@Test
+	public void x1()
+	{
+		System.out.println("This test method of 'X1'");
+	}
+	
+	@Test
+	public void c1()
+	{
+		System.out.println("this method of 'C1' bureau");
+	}
+	
+	@Ignore@Test
+	public void d1()
+	{
+		System.out.println("this is method of d1");
+	}
+
+}
